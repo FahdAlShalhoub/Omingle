5b72203b03c8e6c11a57099af8f22299033db63d
Fahd <FahdShalhoub@gmail.com>
2019-07-03 08:29:16 +0300

    deleted unused packages and deleted old methods in recpetion.js

--- server/Receptionist/reception.js
+++ server/Receptionist/reception.js
@@ -1,21 +1,9 @@
 const matchmaker=require('../Matchmaker/matchmaker');
-const fs=require('fs');
-var waitingList=JSON.parse(fs.readFileSync('server/Receptionist/waitinglist.json','utf-8'));
 
 //Signs the new user to the waiting list. 
 exports.book=function(uuid)
 {
-    if(isValid(uuid)&&!isDuplicated(uuid)){
-        waitingList.uuid.push(uuid); 
-        fs.writeFile('server/Receptionist/waitinglist.json',JSON.stringify(waitingList),(err)=>{
-            if(err){
-            return {"msg":"Oops, Something Went Wrong."}
-            }
-        });
-        return {"msg":"Looking For Chat"};
-    } else {
-        return {"msg":"UUID is invalid"};
-    }
+    
 }
 
 //Checks if the submitted uuid is valid.
@@ -34,35 +22,4 @@ function isValid(uuid)
 
 }
 
-//Checks if the submitted uuid is already in the waiting list.
-function isDuplicated(uuid)
-{
-   for(var i=0;i<waitingList.uuid.length;i++){
-       if(waitingList.uuid[i]==uuid){
-           return true;
-       }
-   }
-}
 
-//Empties The Waiting List.
-function clearWaitingList()
-{
-    var emptyList={"uuid":[]};
-    fs.writeFile('server/Receptionist/waitinglist.json',JSON.stringify(emptyList),(err)=>{
-        if(err){
-        console.log(err);
-        }
-    waitingList=JSON.parse(fs.readFileSync('server/Receptionist/waitinglist.json','utf-8'));
-    });
-
-}
-
-//Passes the waiting list to the matchmaker and empties the waitinglist.json every 5 seconds
-setInterval(()=>{
-    if(waitingList.uuid.length>1){
-    matchmaker.match(waitingList);
-    clearWaitingList();
-    } else{
-        console.log("Not Enough UUID's");
-    }
-},5*1000);
--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
-const express=require("express");
-const app=express();
+const app=require("express")();
 const bodyParser=require('body-parser');
 const router=require('./routes/api');
 const PORT= process.env.PORT||5000;
 
-app.listen(PORT);
+
+app.listen(PORT ,()=>{
+    console.log(`Server is running on port: ${PORT}`)
+});
 
 app.use(bodyParser.urlencoded({extended:true}));
 

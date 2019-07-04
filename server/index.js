d311f74200725ffe7f578252d23fed7863d16cbd
Fahd <FahdShalhoub@gmail.com>
2019-07-04 08:28:28 +0300

    created connection to local mongoDB

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const app=require("express")();
 const bodyParser=require('body-parser');
 const router=require('./routes/api');
 const PORT= process.env.PORT||5000;
+const mongoose=require('mongoose');
+
+//DB Connection
+mongoose.connect('mongodb://localhost/omingle', {useNewUrlParser: true})
+.then(()=>console.log('Connection To DB Successful'))
+.catch(err=>console.log(err));
 
 
 app.listen(PORT ,()=>{

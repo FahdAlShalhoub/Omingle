3ae490e046c9f1009caf019fddc8b94e2d20d02d
Fahd <FahdShalhoub@gmail.com>
2019-07-06 08:09:12 +0300

    deleted unused methods and made the book method promise based

--- server/Receptionist/reception.js
+++ server/Receptionist/reception.js
@@ -1,25 +1,24 @@
-const matchmaker=require('../Matchmaker/matchmaker');
+const booking=require('./booking');
+const sha256=require('sha256');
 
 //Signs the new user to the waiting list. 
 exports.book=function(uuid)
 {
-    
-}
-
-//Checks if the submitted uuid is valid.
-function isValid(uuid)
-{  
-    var uuid=uuid.toString();
-    if(uuid.length==36){
-        if(uuid[8]=="-"&&uuid[13]=="-"&&uuid[18]=="-"&&uuid[23]=="-"){
-            return true;
-        } else{
-            return false
-        }
-    } else {
-        return false;
-    }
-
+   return new Promise((resolve,reject)=>{ 
+        booking.findOne({uuid:uuid})
+        .then(book=>{
+            if(book!=null) reject("Duplicate UUID");
+            const newBooking=new booking({uuid:uuid,perChannel:sha256('uuid')});
+            newBooking.save()
+            .then(()=> resolve("Saved Successfully"))
+            .catch(err=>{
+                reject('err');
+            });
+        })
+        .catch(err=>{
+            reject(err);
+        });
+   });
 }
 
 

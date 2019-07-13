6d4cf29f97d2f3a2ea8cf27f2a10ac3fe7f92e29
Fahd <FahdShalhoub@gmail.com>
2019-07-13 10:10:33 +0300

    Refined book() test

--- server/test/testReception.js
+++ server/test/testReception.js
@@ -1,13 +1,15 @@
 const chai=require('chai').assert;
 const reception=require('../Receptionist/reception');
 const mongoose=require('mongoose');
+const uuid=require('uuid/v4');
 
 mongoose.connect('mongodb://localhost/omingle', {useNewUrlParser: true})
-.then();
+.then(()=>console.log('Connection To DB Successful'))
+.catch(err=>console.log(err));
 
 describe('book()',function(){
     it('Non-duplicated uuid should be saved successfully',()=>{
-        return reception.book('cfd11aba-f107-4ec9-a00c-192819eecc5');
+        return reception.book(uuid());
     });
    
 });

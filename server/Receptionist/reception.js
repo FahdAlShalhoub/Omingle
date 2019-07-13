e5135f3f50e38d9844d1e629002390d3aa98e65c
Fahd <FahdShalhoub@gmail.com>
2019-07-13 10:07:36 +0300

    Refactored Reception

--- server/Receptionist/reception.js
+++ server/Receptionist/reception.js
@@ -2,23 +2,41 @@ const booking=require('./booking');
 const sha256=require('sha256');
 
 //Signs the new user to the waiting list. 
-exports.book=function(uuid)
+exports.book= async function(uuid)
 {
-   return new Promise((resolve,reject)=>{ 
-        booking.findOne({uuid:uuid})
-        .then(book=>{
-            if(book!=null) reject("Duplicate UUID");
-            const newBooking=new booking({uuid:uuid,perChannel:sha256('uuid')});
-            newBooking.save()
-            .then(()=> resolve("Saved Successfully"))
-            .catch(err=>{
-                reject('err');
-            });
-        })
-        .catch(err=>{
-            reject(err);
-        });
-   });
+  let valid = await isValid(uuid);
+  
+  if(valid){
+    const newBooking=new booking({uuid:uuid,perChannel:sha256('uuid')});
+    newBooking.save()
+    .then(msg=>{
+        console.log(msg);
+    })
+    .catch(err=>{
+        throw new Error(err);
+    });
+    return 'Saved Successfully';
+  }
+
+  throw new Error('Duplicate UUID');
+}
+
+async function isValid(uuid)
+{
+    let valid=false;
+
+    await booking.findOne({uuid:uuid})
+    .then(book=>{
+       if(book!=null)
+       valid=false;
+       else
+       valid=true;
+    })
+    .catch(err=>{
+        throw new Error(err);
+    });
+
+    return valid;
 }
 
 

cd4dd2e5febbd5a294a968580d6d8a19e138aedc
Fahd <FahdShalhoub@gmail.com>
2019-07-21 23:15:13 +0300

    added Matchmaker and ChatControl proccesses to the index

--- server/index.js
+++ server/index.js
@@ -3,15 +3,22 @@ const bodyParser=require('body-parser');
 const router=require('./routes/api');
 const PORT= process.env.PORT||5000;
 const mongoose=require('mongoose');
+const http=require('http').createServer(app);
 
 //DB Connection
 mongoose.connect('mongodb://localhost/omingle', {useNewUrlParser: true})
 .then(()=>console.log('Connection To DB Successful'))
 .catch(err=>console.log(err));
 
+//Launching Matchmaker
+const matchmaker=require('./Matchmaker/matchmaker');
+matchmaker.launch(5000);
 
-app.listen(PORT ,()=>{
-    console.log(`Server is running on port: ${PORT}`)
+//Launching ChatControl
+const ChatControl=require('./ChatControl/chatControl')(http);
+
+http.listen(PORT,function(){
+    console.log(`Server is running on port: ${PORT}`);
 });
 
 app.use(bodyParser.urlencoded({extended:true}));

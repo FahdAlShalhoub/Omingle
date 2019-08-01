4bbfd9255fe07fc33fcd0d667969c61a82443ab0
Fahd <FahdShalhoub@gmail.com>
2019-08-01 06:02:07 +0300

    Re-wrote test suite of the reception.js

--- server/test/testReception.js
+++ server/test/testReception.js
@@ -1,60 +1,53 @@
 const chai=require('chai').assert;
+const sinon=require('sinon');
 const reception=require('../Receptionist/reception');
-const mongoose=require('mongoose');
+const booking=require('../Receptionist/booking');
 const uuid=require('uuid/v4');
+const sha256=require('sha256');
 
-before('Create Connection',function(){
-    mongoose.connect('mongodb://localhost/omingle', {useNewUrlParser: true})
-    .then()
-    .catch(err=>console.log(err));
-});
-
-after('Kill Connection',function(){
-    mongoose.disconnect();
-});
-
-describe('isValid',function(){
-    it('Should be true if no duplicates',function(done){
-        reception.isValid(uuid())
-        .then(stat=>{
-            chai.isTrue(stat);
-            done();
-        })
-        .catch(err=>{
-            console.log(err);
-            chai.fail();
-            done();
-        })
-    });
-
-    it('Should be false if duplicates',function(done){
-        reception.isValid('2a1b1b72-8c7e-4c85-926b-1df9f6328a4d')
-        .then(stat=>{
-            chai.isFalse(stat);
-            done();
-        })
-        .catch(err=>{
-            console.log(err);
-            chai.fail();
-            done();
-        })
-    });
-});
-
-describe('book',function(){
-    it('Should book valid uuid',(done)=>{
-        reception.book(uuid())
-        .then(msg=>{
-            chai.equal(msg,'Saved Successfully');
-            done();
-        })
-        .catch(err=>{
-            console.log(err);
-            chai.fail();
-            done();
+describe('testReception.js',function(){
+    describe('book',function(){
+        before('Stubbing booking.exists',function(){
+            sinon
+            .stub(booking,'exists')
+            .resolves(false);
         });
-    });
-   
-});
 
+        after('Stub Deletion',function(){
+            sinon.resetBehavior();
+        });
+
+        beforeEach('Setting up the save spy',function(){
+            sinon.spy(booking,'create');
+        })
+        afterEach('Tearing up the save spy',function(){
+            booking.create.restore();
+        })
+
+        it('Should return a private channel hash',function(done){
+            const id=uuid();
+            const privateChannel=sha256(id);
+            reception.book(id)
+            .then(msg=>{
+                chai.equal(msg,privateChannel);
+                done();
+            })
+            .catch(err=>{
+                done(err);
+            });
+        });
+
+        it('Should save a booking document',function(done){
+            reception.book(uuid())
+            .then(msg=>{
+                chai.isTrue(booking.create.called);
+                done();
+            })
+            .catch(err=>{
+                done(err);
+            });
+        })
+    })
+
+});
 

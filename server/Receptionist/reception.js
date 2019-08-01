e33748b377a2b68118b427b783a26fb383bbee5a
Fahd <FahdShalhoub@gmail.com>
2019-08-01 06:00:25 +0300

    refactored reception by removing isValid and using booking.exists instead

--- server/Receptionist/reception.js
+++ server/Receptionist/reception.js
@@ -4,13 +4,12 @@ const sha256=require('sha256');
 //Signs the new user to the waiting list. 
 async function book(uuid)
 {
-  let valid = await isValid(uuid);
-  
-  if(valid){
+  let exists = await booking.exists({uuid:uuid});
+  if(!exists){
     const channel=sha256(uuid);
-    const newBooking=new booking({uuid:uuid,perChannel:channel});
-    newBooking.save()
-    .then(()=>{})
+    
+    booking.create({uuid:uuid,perChannel:channel})
+    .then()
     .catch(err=>{
         throw new Error(err);
     });
@@ -20,32 +19,7 @@ async function book(uuid)
    throw new Error('Duplicate UUID');
 }
 
-//Checks if the submitted uuid is valid
-async function isValid(uuid)
-{
-    let valid;
-
-    await booking.findOne({uuid:uuid})
-    .then(book=>{
-      if(book==null){
-        valid=true;
-      }
-      else{
-        if(book.uuid===uuid)
-        valid=false;
-        else
-        valid=true;
-      }
-    })
-    .catch(err=>{
-        throw new Error(err);
-    });
-
-    return valid;
-}
-
 module.exports={
-  isValid,
   book
 };
 

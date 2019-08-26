cd7e491ea6b19663912847cf623579f37a0191d5
Fahd <FahdShalhoub@gmail.com>
2019-08-26 03:07:22 +0300

    removed close method and changed module.exports value

--- server/ChatControl/chatControl.js
+++ server/ChatControl/chatControl.js
@@ -1,12 +1,9 @@
-let io=require('socket.io');
-const private=require('./privateChannelManager');
-// let redis=require('socket.io-redis');
-// const public=require('./publicChannelManager');
-
-function launch(http)
-{
-    io=io(http);
-    // redis=redis({host:'localhost',port:'6379'});
+module.exports = function (http){
+    const io=require('socket.io')(http);
+    const redis=require('socket.io-redis')({host:'localhost',port:'6379'});
+    const emitter=require('socket.io-emitter')({host:'127.0.0.1',port:'6379'});
+    const private=require('./privateChannelManager');
+    const public=require('./publicChannelManager').launch(100,emitter);
 
     io.of('/personalCh').on('connection',function(socket){
         socket.on('joinRoom',async function(uuid){
@@ -17,16 +14,14 @@ function launch(http)
     });
 
     io.of('/chatCh').on('connection',function(socket){
+        socket.on('joinRoom',function(chatChannel){
+          socket.join(chatChannel);
+          console.log('User joined public channel'+channel);
+        });
 
+        socket.on('leaveRoom',function(channel){
+            socket.leave(channel);
+            console.log('user disconncted from channel'+channel);
+        });
     });
 }  
-
-function close()
-{
-    io.close();
-}
-
-module.exports={
-    launch,
-    close
-}

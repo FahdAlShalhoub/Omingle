8eaceb04a4300cfe8ae82ca19807160a6cbdf878
Fahd <FahdShalhoub@gmail.com>
2019-08-26 03:09:16 +0300

    increased test's isolation and added one test case

--- server/test/testPrivateChannelManager.js
+++ server/test/testPrivateChannelManager.js
@@ -1,41 +1,50 @@
-const app=require('express')();
-const http=require('http').createServer(app).listen(5000);
-const chatControl=require('../ChatControl/chatControl');
-chatControl.launch(http);
-
 const private=require('../ChatControl/privateChannelManager');
-const client=require('socket.io-client');
 const chai=require('chai').assert;
 const sinon=require('sinon');
 
-describe('privateChannelManager.js',function(){
-
-    before('Stubbing booking.exists and booking.findOne',function(){
-        sinon.stub(booking,'exists')
-        .resolves(true);
-        sinon.stub(booking,'findOne')
-        .resolves({ _id: '5d297a87af7a9d433329b4b5',
-        uuid: '7fc1e382-9f75-4155-bd09-d702ac85ab4b',
-        perChannel:
-        '75666699b77db631f576041e8102a55b46758c01b807eacae0f170930ab59b3d',
-        __v: 0 });
-    });
-    after('Stub Deletion and kill server',function(){
-        chatControl.close();
-        http.close();
-        sinon.restore();
-    });
-
-    it('Should add a socket to a room successfully',function(done){
-        var socket=client.connect('http://localhost:5000/personalCh');
-        socket.on('connect',function(){
-            socket.emit('joinRoom','7fc1e382-9f75-4155-bd09-d702ac85ab4b');
+describe('testPrivateChannelManager.js',function(){
+    describe('addUserToRoom',function(){
+        beforeEach('Stubbing booking.exists and booking.findOne',function(){
+            sinon.stub(booking,'exists')
+            .resolves(true);
+            sinon.stub(booking,'findOne')
+            .resolves({ _id: '5d297a87af7a9d433329b4b5',
+            uuid: '7fc1e382-9f75-4155-bd09-d702ac85ab4b',
+            perChannel:
+            '75666699b77db631f576041e8102a55b46758c01b807eacae0f170930ab59b3d',
+            __v: 0 });
         });
-       
-        socket.on('message',function(msg){
-            chai.equal(msg,'User has joined the channel');
-            done();
+        
+        afterEach('Stub Deletion and kill server',function(){
+            sinon.restore();
         });
+    
+        it('Should add a socket to a room succesfully',function(done){
+            var socket={ join: function(room) {} };
+            sinon.spy(socket,'join');
+            private.addUserToRoom('7fc1e382-9f75-4155-bd09-d702ac85ab4b',socket)
+            .then(()=>{
+                chai.isTrue(socket.join.calledOnceWith('75666699b77db631f576041e8102a55b46758c01b807eacae0f170930ab59b3d'),1);
+                done();
+            })
+            .catch(err=>{
+                done(err);
+            });
+        });
+
+        it('Should Return a message like this: "User has joined the channel" ',function(done){
+            var socket={ join: function(room) {} };
+
+            private.addUserToRoom('7fc1e382-9f75-4155-bd09-d702ac85ab4b',socket)
+            .then(result=>{
+                chai.equal(result,"User has joined the channel");
+                done();
+            })
+            .catch(err=>{
+                done(err);
+            });
+        });
+
     });
 
 });

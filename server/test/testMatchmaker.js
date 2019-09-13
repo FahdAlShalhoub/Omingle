816385304587972f5900ee1f4e2f12f9ac1954f6
Fahd <FahdShalhoub@gmail.com>
2019-09-13 15:26:18 +0300

    replaced a spy a stub for insertMany and deleted checkstatus test cases

--- server/test/testMatchmaker.js
+++ server/test/testMatchmaker.js
@@ -2,7 +2,6 @@ const chai=require('chai').assert;
 const sinon=require('sinon');
 const matchmaker=require('../Matchmaker/matchmaker');
 const match=require('../Matchmaker/match');
-const booking=require('../Receptionist/booking');
 
 describe('testMatchmaker.js',function(){
   describe('makePairs',function(){
@@ -50,57 +49,13 @@ describe('testMatchmaker.js',function(){
       });
   });
 
-  describe('checkStatus',function(){
-    before('Stub Connection',function(){
-      const elements=[ 
-        { _id: '5d297a87af7a9d433329b4b5',
-        uuid: '7fc1e382-9f75-4155-bd09-d702ac85ab4b',
-        perChannel:
-        '75666699b77db631f576041e8102a55b46758c01b807eacae0f170930ab59b3d',
-        __v: 0 },
-      { _id: '5d29808b3f40661da38eb27d',
-        uuid: '5ab2d48d-edc6-403e-9359-e53e350f13bf',
-        perChannel:
-        '75666699b77db631f576041e8102a55b46758c01b807eacae0f170930ab59b3d',
-        __v: 0 },
-      { _id: '5d2980bd3e4484565327c9c6',
-        uuid: '5ab2d48d-edc6-403e-9359-e53e350f13bf',
-        perChannel:
-        '75666699b77db631f576041e8102a55b46758c01b807eacae0f170930ab59b3d',
-        __v: 0 },
-      { _id: '5d2981386ecb665813609c3a',
-        uuid: '5ab2d48d-edc6-403e-9359-e53e350f13bf',
-        perChannel:
-        '75666699b77db631f576041e8102a55b46758c01b807eacae0f170930ab59b3d',
-        __v: 0 },
-        ];
-      sinon
-        .stub(booking,'find')
-        .resolves(elements);
-    });
-    after('Stub Deletion',function(){
-      sinon.restore();
-    });
-
-    it('Should return true if number of elements is even',function(done){
-      matchmaker.checkStatus()
-      .then(stat=>{
-        chai.isTrue(stat);
-        done();
-      })
-      .catch(err=>{
-        done(err);
-      }); 
-    });
-
-  });
-
   describe('saveMatches',function(){
-    beforeEach('Setting up the spy',function(){
-      sinon.spy(match,'insertMany');
+    beforeEach('Setting up the stub',function(){
+      sinon.stub(match,'insertMany')
+      .resolves(2);
     });
     
-    afterEach('Tearing up the spy',function(){
+    afterEach('Stub deletion',function(){
       match.insertMany.restore();
     });
 
@@ -157,5 +112,5 @@ describe('testMatchmaker.js',function(){
       });
     });
   });
-  
+
 });

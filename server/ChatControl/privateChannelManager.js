1b7ec75c79a595f1abf38fabb8684752cc0a61a2
Fahd <FahdShalhoub@gmail.com>
2019-09-13 15:28:00 +0300

    deleted removeUserFromRoom function

--- server/ChatControl/privateChannelManager.js
+++ server/ChatControl/privateChannelManager.js
@@ -18,29 +18,8 @@ const booking=require('../Receptionist/booking');
 
         return result; 
     }
-
-    async function removeUserFromRoom(uuid,socket)
-    {
-        var result;
-        if(! await booking.exists({uuid:uuid})){
-            result = "User Has not made a booking"
-        }
-        booking.findOne({uuid:uuid})
-        .then(booking=>{
-            socket.leave(booking.perChannel);
-            result = "User Has left the channel";
-        })
-        .catch(err=>{
-            throw new Error(err);
-        }); 
-
-        return result;
-    }
-
     module.exports={
         addUserToRoom,
-        removeUserFromRoom
     }
-
 
 
